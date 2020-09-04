d5ec1e3bddbd707e3f7e04a79356cacba42fbc02
Todd Choi <toddchoi36@gmail.com>
2020-09-03 20:27:08 -0400

    one step back

--- application.py
+++ application.py
@@ -37,7 +37,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure postgress
-engine = create_engine("postgres://irqjtbltuuzxab:863ef9dfabdd7b888bc861b52d6c2bf5345dc71bafdcd5f9704b7303dbf7241b@ec2-52-23-86-208.compute-1.amazonaws.com:5432/d7smhfkgvqn5e6")
+engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 # Make sure API key is set
