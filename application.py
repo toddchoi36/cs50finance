0dbbf4193d4eb1e7838a89e395253c5583ee1f10
Todd Choi <toddchoi36@gmail.com>
2020-09-08 18:58:34 -0400

    y

--- application.py
+++ application.py
@@ -306,9 +306,6 @@ def register():
 
 
         if db.execute("SELECT id FROM users WHERE username=:username", {"username": username}).rowcount == 0:
-            primary_key = db.execute("INSERT INTO users(username, hash) VALUES(:username, :hash)", {"username": username, "hash": password})        
-            
-            db.commit()
             return apology("check the tables")
         else:
             return apology("Username already taken")
