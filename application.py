11827fc441073fae6fe61643511a1f54a3737e14
Todd Choi <toddchoi36@gmail.com>
2020-09-08 08:28:20 -0400

    hope

--- application.py
+++ application.py
@@ -12,6 +12,17 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 from helpers import apology, login_required, lookup, usd
 
+import urllib.parse
+import psycopg2
+urllib.parse.uses_netloc.append("postgres")
+url = urllib.parse.urlparse(os.environ["DATABASE_URL"])
+conn = psycopg2.connect(
+ database=url.path[1:],
+ user=url.username,
+ password=url.password,
+ host=url.hostname,
+ port=url.port
+)
 
 # Configure application
 app = Flask(__name__)
