cfedc4488aae24d4221a46c04deb88abeac6fb6f
Todd Choi <toddchoi36@gmail.com>
2020-09-08 18:23:47 -0400

    word

--- application.py
+++ application.py
@@ -306,10 +306,7 @@ def register():
 
 
         if db.execute("SELECT id FROM users WHERE username=:username", {"username": username}).rowcount == 0:
-            primary_key = db.execute("INSERT INTO users(username, hash) VALUES(:username, :hash)", {"username": username, "hash": password})        
-            session["user_id"] = primary_key["id"]
-            db.commit()
-            return redirect("/")
+            return apology("duuuuuuuuuuuude")
         else:
             return apology("Username already taken")
 
