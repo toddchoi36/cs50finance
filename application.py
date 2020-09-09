3194a6dccd59b7c40467c2e29998551d9d7c9e4b
Todd Choi <toddchoi36@gmail.com>
2020-09-09 15:57:08 -0400

    w.e

--- application.py
+++ application.py
@@ -304,10 +304,6 @@ def register():
         username = request.form.get("username")  
         password = generate_password_hash(request.form.get("password"))
 
-
-        if db.execute("SELECT id FROM users WHERE username=:username", {"username": username}).rowcount > 0:
-            return apology("Username already taken")
-
         db.execute("INSERT INTO users(username, hash) VALUES(:username, :hash)", {"username": username, "hash": password})        
         db.commit()
         return render_template("login.html")
