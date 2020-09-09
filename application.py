04548d401407780eb6e5c0b7e70bc6df3c0c2498
Todd Choi <toddchoi36@gmail.com>
2020-09-09 15:59:04 -0400

    me

--- application.py
+++ application.py
@@ -304,8 +304,6 @@ def register():
         username = request.form.get("username")  
         password = generate_password_hash(request.form.get("password"))
 
-        db.execute("INSERT INTO users(username, hash) VALUES(:username, :hash)", {"username": username, "hash": password})        
-        db.commit()
         return render_template("login.html")
 
 
