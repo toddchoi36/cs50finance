5a71175105308f9ed805559ad8261303b393db6b
Todd Choi <toddchoi36@gmail.com>
2020-09-09 19:56:36 -0400

    yy

--- application.py
+++ application.py
@@ -308,7 +308,7 @@ def register():
             user = db.execute("SELECT id FROM users WHERE username =:username", {"username": username})
             db.commit()
             session["user_id"] = user[0]["id"]
-            return render_template("/")
+            return redirect("/")
         else:
             return apology("Username already taken")
         
