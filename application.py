7d9ab251ce7072c4d0128c1b9afd48386e168b07
Todd Choi <toddchoi36@gmail.com>
2020-09-10 17:07:13 -0400

    test

--- application.py
+++ application.py
@@ -216,7 +216,7 @@ def login():
 
         # Query database for username
         rows = db.execute("SELECT * FROM users WHERE username = :username",
-                        username=request.form.get("username"))
+                        {"username": request.form.get("username")})
 
         # Ensure username exists and password is correct
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
@@ -226,7 +226,7 @@ def login():
         session["user_id"] = rows[0]["id"]
 
         # Redirect user to home page
-        return redirect("/")
+        return render_template("login.html")
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
