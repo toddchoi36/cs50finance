0721762d09f472f7d146f771c43a053738733571
Todd Choi <toddchoi36@gmail.com>
2020-09-11 16:07:34 -0400

    session

--- application.py
+++ application.py
@@ -34,6 +34,7 @@ app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
+Session = scoped_session(sessionmaker(bind=engine)
 
 # Configure postgress
 engine = create_engine("postgres://irqjtbltuuzxab:863ef9dfabdd7b888bc861b52d6c2bf5345dc71bafdcd5f9704b7303dbf7241b@ec2-52-23-86-208.compute-1.amazonaws.com:5432/d7smhfkgvqn5e6")
