92bd40c8e56ff69329c001add7d2e359b01d7250
Todd Choi <toddchoi36@gmail.com>
2020-09-11 17:43:39 -0400

    sfs

--- application.py
+++ application.py
@@ -217,7 +217,7 @@ def login():
 
         # Query database for username
         rows = db.execute("SELECT * FROM users WHERE username = :username",
-                        username=request.form.get("username"))
+                        {"username":request.form.get("username")})
 
         # Ensure username exists and password is correct
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
